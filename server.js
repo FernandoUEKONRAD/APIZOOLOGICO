2f1920bfe0ee6ce5a83e65b61c85ae2a636df620
Sara <saravaldes.0719@gmail.com>
2026-05-07 19:26:26 -0500

    7 de mayo

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 });
 
 // Puerto de conexión
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
